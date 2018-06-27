fa35219f5ffee7fbc0d9bbfc596815cb67e58bf7
omairshamshir <omairshamshir@gmail.com>
2018-06-28 00:31:31 +0500

    added new build

--- user_manager/static/ui/src/HomePage/HomePage.jsx
+++ user_manager/static/ui/src/HomePage/HomePage.jsx
@@ -16,26 +16,13 @@ class HomePage extends React.Component {
     render() {
         const { user, users } = this.props;
         return (
-            <div className="col-md-6 col-md-offset-3">
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
-                <h3>All registered users:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
-                            </li>
-                        )}
-                    </ul>
-                }
+            <div className="col-md-9 col-md-offset-2">
+              
+              
+                <h3>Hi {user.email}!</h3>
+                {!user.is_activated && <div className="alert alert-danger">Your account is not activated, please follow the link in your email to activate</div>}
+                <p>Welcome Home! </p>
+              
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
